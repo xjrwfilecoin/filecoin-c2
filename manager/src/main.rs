1a593e17bea5debf712b2e11007bb213ae3c0ad5
Unknown <unknown@example.com>
2020-09-19 14:30:24 +0800

    add demo

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -1,3 +1,13 @@
+use jsonrpc_ws_server::*;
+use jsonrpc_ws_server::jsonrpc_core::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut io = IoHandler::new();
+    io.add_method("say_hello", |_params| {
+        Ok(Value::String("hello".into()))
+    });
+    let server = ServerBuilder::new(io)
+        .start(&"0.0.0.0:3030".parse().unwrap())
+        .expect("Server must start with no issues");
+    server.wait().unwrap()
 }
